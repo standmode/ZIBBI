c781f70f0e5b45a7299c8c6e4d3f4157763f9c4a
standmode <149881876+standmode@users.noreply.github.com>
2023-11-08 20:18:37 +0300

    Add files via upload

---
+++ ZIBBI.py
@@ -0,0 +1,201 @@
+import time
+print("     ZIBBI 2.0     ")
+print(" TG @HACKER_PHONE_VIP")
+print("[1] Гео по номеру")
+print("[2] Взлом Vk")
+print("[4] гео по Ip")
+print("[3] Взлом Telegram")
+print("[5] Гео по Telegram")
+print("[6] взлом Instagram")
+print("[7] Спам по номеру")
+print("[8] взлом QIWI")
+print("[9] спам в Telegram")
+print("[10] Взлом PayPal")
+print("[11] Докс по номеру")
+print("[12] Взлом Google")
+print("[13] Докс по Telegram")
+print("[14] взлом Steam")
+print("[15] Докс по gmail]")
+print("[16] взлом PUBG M.")
+print("[17] Ddos сайтов")
+print("[18] Взлом CoD M.")
+print("[19] Ddos ip")
+print("[20] Взлом BrawlStars")
+print("[21] Ddos minecraft")
+print("[22] Взлом Clash Royale")
+print("[23] Взлом WIFI")
+print("[24] Взлом P0rnHub")
+print("[25] Взлом Э.Дневник")
+print("[26] Взлом Кундалик")
+print("[27] Фио по номеру")
+print("[28] Взлом мир танков")
+print("[29] Номер по фио")
+print("[30] Взлом Юмани")
+print("[31] Сват По номеру")
+print("[32] взлом WhatsApp")
+print(" ")
+print("[0] Выход")
+print("")
+print("     ZIBBI")
+b = 1
+x = 0
+g = 0
+x = int(input())
+if x == 1:
+	c = input("Введите гео ")
+	print("поиск...")
+	time.sleep(3)
+	print("Анализ данных...")
+	while b == 1:
+		g = input()
+if x == 2:
+	c = input("Введите номер")
+	print("Взлом "+ c)
+	while b == 1:
+		g = input()
+if x == 3:
+	c = input("Введите номер ")
+	print("Взлом " + c)
+	while b == 1:
+		g = input()
+if x == 4:
+	c = input("Введите ip ")
+	print("Анализ...")
+	while b == 1:
+		g = input()
+if x == 5:
+	c = input("Введите username ")
+	print("узнаем гео " + c)
+	while b == 1:
+		g = input()
+if x == 6:
+		c = input("Введите username ")
+		print("Взлом " + c)
+		while b == 1:
+			g = input()
+if x == 7:
+		c = input("Введите номер ")
+		print("Отпровляем смс на номер " + c)
+		while b == 1:
+			g = input()
+if x == 8:
+	c = input("Введите номер ")
+	print("Взламываем киви кошелек " + c)
+if x == 9:
+	c = input("Введите username ")
+	print("Спамим " + c)
+	while b == 1:
+		g = input()
+if x == 10:
+	c = input("Введите данные ")
+	print("Взлом " + c)
+	while b == 1:
+		g = input()
+if x == 11:
+	c = input("Введите номер телефона ")
+	print("узнаем данные " + c)
+	while b == 1:
+		g = input()
+if x == 12:
+	c = input("Введите почту ")
+	print("Взлом " + c)
+if x == 13:
+	c = input("Введите username ")
+	print("Ищем информацию про " + c)
+	while b == 1:
+		q = input()
+if x == 14:
+	c = input("Введите user name ")
+	print("Взлом " + c)
+	while b == 1:
+		q = input()
+if x == 15:
+	c = input("Введите почту gmail " + c)
+	print("Ищем информацию о "+ c)
+	while b == 1:
+		q = input()
+if x == 16:
+	c = input("Введите Uid")
+	print("Взлом " + c)
+if x == 17:
+	c = input("Введите url сайта ")
+	print("send to " + c)
+	while b == 1:
+		q = input()
+if x == 18:
+	c = input("Введите данные ")
+	print("Взлом " + c)
+	while b == 1:
+		q = input()
+if x == 19:
+	c = input("Введиет ip ")
+	print("атакуем " + c)
+	while b == 1:
+		q = input()
+if x == 20:
+	c = input("Введете почту ")
+	print("Взлом" + c)
+	while b == 1:
+		q = input()
+if x == 21:
+	c = input("Введиет ip server ")
+	print("Ddos to " + c)
+	while b == 1:
+		q = input()
+if x == 22:
+	c = input("Введите данные ")
+	print("взлом")
+	while b == 1:
+		q = input()
+if x == 23:
+	c = input("Введите имя сети WIFI ")
+	print("Подборка поролей для " + c)
+	while b == 1:
+		q = input()
+if x == 24:
+	c = input("Введите имя пользователя ")
+	print("Взлом " + c)
+	while b == 1:
+		q = input()
+if x == 25:
+	c = input("Введите ссылку на эл лневник ")
+	print("Взлом " + c)
+	while b == 1:
+		q = input()
+if x == 26:
+	c = input("Введите данные ")
+	print("Взлом")
+	while b == 1:
+		q = input()
+if x == 27:
+	c = input("Введите номер ")
+	print("ищем данные")
+	while b == 1:
+		q = input()
+if x == 28:
+	c = input("Введите данные")
+	print("Взламываем " + c)
+	while b == 1:
+		q = input()
+if x == 29:
+	c = input("Введите фио ")
+	print("Ищем данные о " + c)
+	while b == 1:
+		q = input()
+if x == 30:
+	c = input("Введите данные")
+	print("Взлом..")
+	while b == 1:
+		q = input()
+if x == 31:
+	c = input("введите номер ")
+	print("Сваттинг " + c)
+	while b == 1:
+		q = input()
+if x == 32:
+	c = input("Введите номер ")
+	print("Взлом " + c)
+	while b == 1:
+		q = input()
+if x == 0:
+	exit
